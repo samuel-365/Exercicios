ebe9cc45a3b7ef0aee874a594980acace56570bc
samuelsv365 <72899008+samuel-365@users.noreply.github.com>
2022-05-06 18:52:46 -0300

    mib-internacional project added

---
+++ mib-international/javascript.js
@@ -0,0 +1,8 @@
+const tessa = document.querySelector('.info')
+
+function tessaBtt(){
+    tessa.classList.add('active');
+}
+function closeInfo(){
+    tessa.classList.remove('active');
+}
---
+++ mib-international/teste/script.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector('.modal-container')
+
+function openModal(){
+    modal.classList.add('active')
+}
+function closeModal(){
+    modal.classList.remove('active')
+}
